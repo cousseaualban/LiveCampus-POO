d8047f56694638a633f6cc08e97e5a21f1bf090b
cousseaualban <cousseaualban@gmail.com>
2025-02-12 14:06:15 +0100

    Projet POO page d'authentification

---
+++ Views/auth.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./css/auth.css" type="text/css" />
+</head>
+<body>
+    <div class="container">
+        <h2>Connexion</h2>
+        <form id="loginForm">
+            <input type="text" id="username" placeholder="Nom d'utilisateur *" required>
+            <input type="password" id="password" placeholder="Mot de passe *" required>
+            <button type="submit">Se connecter</button>
+        </form>
+    </div>
+</body>
+</html>
